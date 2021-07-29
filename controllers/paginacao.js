58faedc5931eb350b9bd2311c7a8889d8eded3bd
Vinicius <viniciusdelima54@gmail.com>
2021-07-29 18:44:14 -0300

    Correção de erro

--- controllers/paginacao.js
+++ controllers/paginacao.js
@@ -3,40 +3,45 @@ module.exports = app => {
     app.get('/paginacao', (req, res) => {
         var body = (req.body);
 
-        if (!body.pagina || !body.totalPagina) {
+        if (!body.id || !body.paginacao) {
             res.statusCode = 400;
-            res.send('Ocorreu um erro com o layout recebido, mande na seguinte forma: {"pagina":  1,"totalPagina": 30}')
+            res.send('Ocorreu um erro com o layout recebido, mande na seguinte forma: {"pagina":  1,"paginacao": 30}')
         }
         else {
-            if (body.totalPagina <= 5) { //REGRA PARA PAGINA TOTAL DE ITENS MENOR QUE 5
+            if (body.paginacao <= 5) { //REGRA PARA PAGINA TOTAL DE ITENS MENOR QUE 5
                 let response = []
 
-                for (let i = 1; i <= body.totalPagina; i++) {
-                    response.push("" + i);
+                for (let i = 1; i <= body.paginacao; i++) {
+                    if (i == body.id) {
+                        response.push("**" + i + "**");
+                    }
+                    else {
+                        response.push("" + i);
+                    }
                 }
 
                 res.send(response);
             }
-            else if (body.totalPagina >= 6) {
+            else if (body.paginacao >= 6) {
                 //-------------------------------------------------------
                 //LOGICA PARA LIMITAR O NUMERO MAXIMO E MINIMO DE PAGINAS
                 //-------------------------------------------------------
 
-                var limitadorMax = body.pagina + 2;
-                var limitadorMin = body.pagina - 2;
+                var limitadorMax = body.id + 2;
+                var limitadorMin = body.id - 2;
 
-                if (limitadorMax >= body.totalPagina) {
+                if (limitadorMax >= body.paginacao) {
                     limitadorMin -= 2;
-                    limitadorMax = body.totalPagina;
+                    limitadorMax = body.paginacao;
 
-                    limitadorMin += limitadorMax - body.pagina;
+                    limitadorMin += limitadorMax - body.id;
                 }
 
                 if (limitadorMin <= 1) {
                     limitadorMin = 1
                     limitadorMax += 2;
 
-                    limitadorMax += limitadorMin - body.pagina;
+                    limitadorMax += limitadorMin - body.id;
                 }
                 //-----------------------------------------------------------------
                 //FAZ AS ULTIMAS TRATIVAS DOS ITENS SEGUINDO AS REGRAS APRESENTADAS
@@ -45,7 +50,7 @@ module.exports = app => {
                 let response = []
 
                 for (let i = limitadorMin; i <= limitadorMax; i++) {
-                    if (i == body.pagina) {
+                    if (i == body.id) {
                         response.push("**" + i + "**");
                     }
                     else {
@@ -55,7 +60,10 @@ module.exports = app => {
                 if (response[0] > 1) {
                     response.unshift('...');
                 }
-                if (response[5] < 10 && response[4] < 10) {
+                if (response[4] < body.paginacao && !response[5]) {
+                    response.push('...');
+                }
+                if (response[4] < body.paginacao && response[5] < body.paginacao) {
                     response.push('...');
                 }
 
@@ -66,7 +74,7 @@ module.exports = app => {
             //-----------------------------------------------------------------
 
             else {//CASO DE ALGUM ERRO INESPERADO PARA NÃO DEIXAR SEM RESPOSTA
-                res.statusCode(404)
+                res.statusCode = 404
                 res.send("Ocorreu um erro ao processar sua requisição!");
             }
         }
